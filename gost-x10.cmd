2913300f615b6cc6b82ded9f484d53bef5ec6b64
Kevin Tang <tangkv@users.noreply.github.com>
2019-07-08 09:21:05 +0800

    Add files via upload

---
+++ gost-x10.cmd
@@ -0,0 +1,2 @@
+@title gost 8899 to heroku single app x10
+@gost.exe -L=:8899 -F=http://***.***.***.***:***?agent=note -F=ss+mws://**user**:***pass***@**your**app**name.herokuapp.com:80?ip=gost-x10.txt
