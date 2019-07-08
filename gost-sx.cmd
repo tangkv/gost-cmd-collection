45b60aa46bb7474f83078afe4995981ad40dc394
Kevin Tang <tangkv@users.noreply.github.com>
2019-07-08 13:47:31 +0800

    Add files via upload

---
+++ gost-sx.cmd
@@ -0,0 +1,2 @@
+title gost @ 8888 via xxx.xxx.xxx.xxx:xxx to Heroku endpoints x5
+gost.exe -L=:8888 -F=http://xxx.xxx.xxx.xxx:xxx?agent=Note -F=ss+mws://method:key@yourapp.herokuapp.com:80?peer=gost-sx.json
